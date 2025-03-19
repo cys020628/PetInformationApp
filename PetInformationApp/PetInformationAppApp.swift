e42f45c5acbc5397ba318eb59708522562608710
youngseoc <cys020628@naver.com>
2025-03-19 19:49:05 +0900

    Initial Commit

---
+++ PetInformationApp/PetInformationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetInformationAppApp.swift
+//  PetInformationApp
+//
+//  Created by 영석 on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PetInformationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetInformationAppTests/PetInformationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetInformationAppTests.swift
+//  PetInformationAppTests
+//
+//  Created by 영석 on 3/19/25.
+//
+
+import Testing
+@testable import PetInformationApp
+
+struct PetInformationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
